35529a055fdf7a7122a818cf9d838d11a00f7f44
javonnep <javonne.porter@ymail.com>
2022-02-21 23:48:56 +0000

    added test rlearn file with callbacks for city hpc thing

--- build/PRACTICE STUFF/src/sbtest.py
+++ build/PRACTICE STUFF/src/sbtest.py
@@ -1,19 +1,31 @@
 import gym
-
+import time
+import tensorflow as tf
 from stable_baselines3 import PPO, A2C
 
 env = gym.make("CartPole-v1")
+filepath="TESTCALLBACK"
+cb = tf.keras.callbacks.ModelCheckpoint(
+    filepath, monitor='accuracy'
+)
 
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=60_000)
+steps = 70_000
+for i in range(steps):
+    model.learn(total_timesteps=steps)
+    if steps % 5000 == 0:
+        print("check")
+        model.save(f"{filepath}/model{time.time().__round__(0)}")
 
 obs = env.reset()
-for i in range(10_000):
+# for i in range(10_000):
+for i in range(10):
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
     env.render()
     if done:
         obs = env.reset()
         print("CLOSING")
+
 
 env.close()
